82805a64edcc238b23c2ce247ab2f11949964dfc
Jens Neuse <jens.neuse@gmx.de>
2021-05-04 15:19:11 +0200

    add apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'https://api.spacex.land/graphql/',
+    cache: new InMemoryCache()
+});
+
+function MyApp({Component, pageProps}) {
+    return (
+        <ApolloProvider client={client}>
+            <Component {...pageProps} />
+        </ApolloProvider>
+    )
 }
 
 export default MyApp
